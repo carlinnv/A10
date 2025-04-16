8de00f938357db7fe13f536ccee9932778ac9cbf
Carlin <carlinnv@gmail.com>
2025-04-15 22:07:47 -0400

    Changed cookies.js script

--- cookies.js
+++ cookies.js
@@ -1,15 +1,19 @@
-document.cookie = "name=Carlin; SameSite=None; Secure";
-document.cookie = "favorite_food=tripe; SameSite=None; Secure";
+document.cookie = "visited=0; SameSite=None; Secure";
+
+const timesVisited = 0; 
 
 function showCookies() {
     const output = document.getElementById("cookies"); 
     output.textContent = `> ${document.cookie}`;
 }
 
-function clearOutputCookies() {
-    const output = document.getElementById("cookies"); 
-    output.textContent = ""; 
+function addToCookies() {
+    timesVisited+=1; 
+    document.cookie= "visited=" + timesVisited + "; SameSite=None; Secure"; 
 }
 
-console.log("hello"); 
-console.log(document.cookie); 
+function clearOutputCookies() {
+    const output = document.getElementById("cookies"); 
+    timesVisited = 0; 
+    output.textContent = ""; 
+}
