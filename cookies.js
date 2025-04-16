f41b256c0d8224add283e0b134c1a760be95241c
Carlin <carlinnv@gmail.com>
2025-04-15 22:16:14 -0400

    fixed js file

--- cookies.js
+++ cookies.js
@@ -10,6 +10,8 @@ function showCookies() {
 function addToCookies() {
     timesVisited+=1; 
     document.cookie= "visited=" + timesVisited + "; SameSite=None; Secure"; 
+    const output = document.getElementById("cookies"); 
+    output.textContent = `> ${document.cookie}`;
 }
 
 function clearOutputCookies() {
